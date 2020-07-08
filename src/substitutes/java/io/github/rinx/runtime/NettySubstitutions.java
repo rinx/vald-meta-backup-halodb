e4a1750fd1be86cf8c9172e5aedc4ae227d0afcb
Rintaro Okamura <rintaro.okamura@gmail.com>
2020-07-08 22:58:15 +0900

    :wrench: add substitution for NativeLibraryLoader
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- src/substitutes/java/io/github/rinx/runtime/NettySubstitutions.java
+++ src/substitutes/java/io/github/rinx/runtime/NettySubstitutions.java
@@ -9,6 +9,19 @@ import com.oracle.svm.core.annotate.TargetClass;
 
 import java.util.function.Predicate;
 
+
+@TargetClass(className = "io.netty.util.internal.NativeLibraryLoader")
+final class Target_io_netty_util_internal_NativeLibraryLoader {
+
+    // This method can trick GraalVM into thinking that Classloader#defineClass is getting called
+    @Substitute
+    static Class<?> tryToLoadClass(final ClassLoader loader, final Class<?> helper)
+            throws ClassNotFoundException {
+        return Class.forName(helper.getName(), false, loader);
+    }
+
+}
+
 @TargetClass(className = "io.netty.buffer.AbstractReferenceCountedByteBuf", onlyWith = PlatformHasClass.class)
 final class Target_io_netty_buffer_AbstractReferenceCountedByteBuf {
     @Alias
